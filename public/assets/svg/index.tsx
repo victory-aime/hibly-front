0f799dc5d51ff1f2440f93550c3ee8c13c7ca961
victory-aime <victorydarnelmbenze@gmail.com>
2025-07-25 14:23:43 +0100

    fix(authentication): next auth auth logic
    correct refresh token function and update error modal components

--- public/assets/svg/index.tsx
+++ public/assets/svg/index.tsx
@@ -1,14 +1,25 @@
-import ListMenu from './list-menu.svg'
-import HomeIcon from './home-v2.svg'
-import ArrowDown from './arrow-down.svg'
-import Dot from './dot.svg'
-import LogOutIcon from './log-out.svg'
-import EyeOff from './eye-off-icon.svg'
-import Eye from './eye-icon.svg'
-import TrashIcon from './trash-icon.svg'
-import CarsIcon from './cars.svg'
-import ParkingIcon from './parking.svg'
-import LockIcon from './lock.svg'
-import GroupIcon from './group-line.svg'
+import ListMenu from './list-menu.svg';
+import HomeIcon from './home-v2.svg';
+import ArrowDown from './arrow-down.svg';
+import Dot from './dot.svg';
+import LogOutIcon from './log-out.svg';
+import EyeOff from './eye-off-icon.svg';
+import Eye from './eye-icon.svg';
+import TrashIcon from './trash-icon.svg';
+import CarsIcon from './cars.svg';
+import ParkingIcon from './parking.svg';
+import LockIcon from './lock.svg';
 
-export { ListMenu, LockIcon, Dot, LogOutIcon, ArrowDown, HomeIcon, Eye, EyeOff, TrashIcon, CarsIcon, ParkingIcon, GroupIcon }
+export {
+  ListMenu,
+  LockIcon,
+  Dot,
+  LogOutIcon,
+  ArrowDown,
+  HomeIcon,
+  Eye,
+  EyeOff,
+  TrashIcon,
+  CarsIcon,
+  ParkingIcon,
+};
